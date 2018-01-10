8584d0eff49f09a06e51cf16db8f3506a76f5edc
armanaxf <35300856+armanaxf@users.noreply.github.com>
2018-01-10 15:59:10 +0000

    Removed Script
    Updated script to include connectivety to O365 POSH

---
+++ ConnecttoO365.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy RemoteSigned
+ $Credential = Get-Credential
+ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
+ Import-PSSession $Session
+ Connect-MsolService -Credential $Credential
