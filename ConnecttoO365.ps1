97b8dad7d757c592b6ac3120d13d3fdfb7e5f3c8
armanaxf <35300856+armanaxf@users.noreply.github.com>
2018-01-19 13:44:46 +0000

    deleted
    deleted

--- ConnecttoO365.ps1
+++
@@ -1,5 +0,0 @@
-﻿Set-ExecutionPolicy RemoteSigned
- $Credential = Get-Credential
- $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
- Import-PSSession $Session
- Connect-MsolService -Credential $Credential
---
+++ O365tools.psm1
@@ -0,0 +1,43 @@
+﻿function new-o365session {
+    Set-ExecutionPolicy RemoteSigned
+    $Credential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+    Import-Module (Import-PSSession $Session)
+}
+
+function Connect-O365 {
+    $o365cred = Get-Credential 
+    $session365 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Credential $o365cred -Authentication Basic -AllowRedirection 
+    Import-Module (Import-PSSession $session365 -AllowClobber) -Global
+    connect-msolservice -Credential $o365cred
+}
+
+function new-365user
+{
+[cmdletbinding()]
+Param(
+[parameter(Mandatory=$True)]
+ [String]$firstname,
+ [String]$lastname,
+ [string]$email,
+ [parameter(Mandatory=$False)]
+ [String]$Groups
+)
+
+ $fullname = "$firstname $lastname"
+
+ New-msoluser -UserPrincipalName $email -DisplayName $fullname -FirstName $firstname -LastName $lastname -UsageLocation "GB"
+ set-MailboxRegionalConfiguration -identity $email -Language en-GB -TimeZone "GMT Standard Time" -DateFormat dd-MMM-yy -Timeformat HH:mm
+
+ }
+
+ function add-365license
+{
+[cmdletbinding()]
+Param(
+[parameter(mandatory=$True)]
+ [String]$license,
+ [String]$email
+)
+ set-msoluserlicense -addlicenses $License -userprincipalname $email
+ }
