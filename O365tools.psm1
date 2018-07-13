f6dd2545396b9cd4cdab6c289621cfe40c7fb62f
armanaxf <35300856+armanaxf@users.noreply.github.com>
2018-07-13 11:55:59 +0100

    added comment based help
    added comment based help to  tols omake iteasier to use

--- O365tools.psm1
+++ O365tools.psm1
@@ -1,18 +1,27 @@
-﻿<#
+﻿function Connect-O365 {
+    <#
 .Synopsis
 Conencts to O365 and Exchange Online.
 .DESCRIPTION
 Establishes a simultaneous connection to O365 and Exchange Online Powershell
+.EXAMPLE
+Connect-O365
 #>
-function Connect-O365 {
     $o365cred = Get-Credential 
     $session365 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Credential $o365cred -Authentication Basic -AllowRedirection 
     Import-Module (Import-PSSession $session365 -AllowClobber) -Global
     connect-msolservice -Credential $o365cred
 }
 
-function new-365user
-{
+function new-365user {
+    <#
+.SYNOPSIS
+Creates a new O365 user.
+.DESCRIPTION
+Creates a new user specifying the First/Lastname and UPN
+.EXAMPLE
+new-365user -firstname Joe -Lastname Blogs -email joe.blogs@abc.com.
+#>
 [cmdletbinding()]
 Param(
 [parameter(Mandatory=$True)]
@@ -31,6 +40,14 @@ Param(
  }
 
  function add-365license
+ <#
+ .SYNOPSIS
+ Adds a license to the user
+ .DESCRIPTION 
+ Adds the license to the user
+ .NOTES
+ Work In progress 
+ #>
 {
 [cmdletbinding()]
 Param(
